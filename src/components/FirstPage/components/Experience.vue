9ffa26154aa26bf3be165bc2eebe290b211dbd2d
VIJAY <vijay.r@fcsonline.co.in>
2025-04-28 11:40:44 +0530

    added new fix in timeline

--- src/components/FirstPage/components/Experience.vue
+++ src/components/FirstPage/components/Experience.vue
@@ -19,8 +19,8 @@
         </v-col>
       </v-row>
       <v-row class="d-flex justify-center">
-        <v-timeline align-top dense>
-          <v-timeline-item v-for="(experience, i) in experiences" :key="i" :color="experience.color || 'teal'" large>
+        <v-timeline align-top dense >
+          <v-timeline-item small v-for="(experience, i) in experiences" :key="i" :color="experience.color || 'teal'" large>
             <template v-slot:icon>
               <v-avatar>
                 <v-icon :color="experience.color || 'teal'">{{ experience.icon }}</v-icon>
